23a00284adfa7a0e2be1dff5e5d71de686aa63d7
ibbya42 <ibbyali4214@gmail.com>
2023-11-21 14:42:57 +1300

    21/11/23 Modal Push

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // template_32hlc8s - Template id 
 // service_hhryq49 - Service id 
-// d7Cu_g45j2yUzsH7p - Public id 
+// JZuJGDHwdqUsCxxe9 - Public id 
 
 
-function contact(){
-    console.log("It worked")
+function contact(event){
+    event.preventDefault();
+    // emailjs
+    //     .sendForm(
+    //         'service_hhryq49',
+    //         'template_32hlc8s',
+    //         event.target,
+    //         'JZuJGDHwdqUsCxxe9'
+    //     ).then(() => {
+    //         console.log("It worked")
+    //     })
+
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible'
+    
+    setTimeout(() => {
+        loading.classList.remove('modal__overlay--visible')
+        success.classList += ' modal__overlay--visible'
+        console.log("It worked")
+    }, 1000);
 }
